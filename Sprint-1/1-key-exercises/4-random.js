5b089054d02299db0a826bf6543f9eabac4a66e8
burhan-mustafa <m-b.mustafa@outlook.com>
2026-06-12 23:14:35 +0100

    section 1 is now complete and section 2 task 1 is complete

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,5 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+//Answer: Firstly a random number that is > or = to = 0 BUT less than 1 is generated, that number is multiplied a 100 and then always rounded down to the nearest whole number and then the minimum value of 1 is added
