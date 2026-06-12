c921f4a99a22cd18001d4833dfca8c8f316804bc
burhan-mustafa <m-b.mustafa@outlook.com>
2026-06-12 23:31:43 +0100

    sprint 2 exercises 1 & 2 completed

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,6 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
+// We were trying to call a the variable before it was even initialized / made.
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
