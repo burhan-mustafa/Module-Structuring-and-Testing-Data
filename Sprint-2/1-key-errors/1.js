1f4dc42abef9507cadaf4512f82cd0c7b602909e
burhan-mustafa <m-b.mustafa@outlook.com>
2026-06-24 18:03:02 +0100

    Sprint 2 Task 2 completed

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -2,9 +2,6 @@
 
 // Why will an error occur when this program runs?
 // =============> write your prediction here
-
-// Try playing computer with the example to work out what is going on
-
 function convertToPercentage(decimalNumber) {
   const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
@@ -14,7 +11,19 @@ function convertToPercentage(decimalNumber) {
 
 console.log(decimalNumber);
 
+// Try playing computer with the example to work out what is going on
+
 // =============> write your explanation here
+// Since the parameter and variable have the same name, decimalNumber, we will get an error as we can not declare a
+// variable of the same name with in the functions local scope
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+
+  return percentage;
+}
+
+console.log(convertToPercentage(0.5));
