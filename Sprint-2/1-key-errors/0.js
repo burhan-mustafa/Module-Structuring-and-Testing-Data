514c086fe67e0960ca8247e3843b1a8047072cba
burhan-mustafa <m-b.mustafa@outlook.com>
2026-06-24 18:08:16 +0100

    Sprint 2 Task 0 completed

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,8 +1,10 @@
 // Predict and explain first...
-//  =============> write your prediction here
+// =============> write your prediction here
+// An error will be thrown because str has already been declared as the function's parameter.
+// The function parameter and the let variable are both declared in the same function scope.
 
-// call the function capitalise with a string input
-// interpret the error message and figure out why an error is occurring
+// Call the function capitalise with a string input.
+// Interpret the error message and figure out why an error is occurring.
 
 function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
@@ -10,4 +12,15 @@ function capitalise(str) {
 }
 
 // =============> write your explanation here
+// The error occurs because str is already declared as the function's parameter, so it cannot be
+// declared again using let inside the same scope. By changing the new variable's name to
+// capitalisedStr, there is no longer a naming conflict and the code runs without any errors.
+
 // =============> write your new code here
+
+function capitalise(str) {
+  let capitalisedStr = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return capitalisedStr;
+}
+
+console.log(capitalise("mmm"));
