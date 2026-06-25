329448e5319692a0de1a71b1b43e4a9bab616925
burhan-mustafa <m-b.mustafa@outlook.com>
2026-06-25 19:43:30 +0100

    sprint 2 task 2 exercise 1 completed

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// I expect that we will get an undefined output on line 10 since the function does not return anything
 
 function sum(a, b) {
   return;
@@ -9,5 +10,13 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+// The function sum defines that a+b must be added together but before that return is used with no argument defined and we we exit the function immediately when it is called on line 10 the function returns to the string undefined.
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
