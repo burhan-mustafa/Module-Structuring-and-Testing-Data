29057036bf4134d0195d1a5b4904ff4368a2b6f2
burhan-mustafa <m-b.mustafa@outlook.com>
2026-06-25 13:43:12 +0100

    Sprint 2 Task 2 completed

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,25 @@
-
 // Predict and explain first BEFORE you run any code...
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// =============> write your prediction of the error here:
+// we have used a number instead of naming the parameter so we should get a syntax error
 
 function square(3) {
-    return num * num;
+  return num * num;
 }
 
 // =============> write the error message here
+// SyntaxError: Unexpected number
 
 // =============> explain this error message here
+// JavaScript read the number 3 as a number rather than the name of the parameter.
+// Parameters must have valid names
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
-
+function square(num) {
+  return num * num;
+}
